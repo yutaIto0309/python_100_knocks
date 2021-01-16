45912deb418501d60a86432af78424888308e08b
yutaIto0309 <yokosingo@gmail.com>
2021-01-16 16:42:22 +0900

    二章

--- 2章/knock20.py
+++ 2章/knock20.py
@@ -27,3 +27,31 @@ for trg in list(uriage_data.loc[flg_is_null, 'item_name'].unique()):
 uriage_data.head()
 # %%
 uriage_data.isnull().any()
+# %%
+# ノック16
+kokyaku_data['顧客名'].head()
+# %%
+uriage_data['customer_name'].head()
+# %%
+# 顧客名から余分なスペースを除く
+kokyaku_data['顧客名'] = kokyaku_data['顧客名'].str.replace(" ", "")
+kokyaku_data['顧客名'] = kokyaku_data['顧客名'].str.replace("　", "")
+# %%
+# ノック17
+flg_is_digit = kokyaku_data['登録日'].astype('str').str.isdigit()
+flg_is_digit
+# %%
+flg_is_serial = pd.to_timedelta(kokyaku_data.loc[flg_is_digit, '登録日'], unit='D')
+from_serial = flg_is_serial + pd.to_datetime('1900/01/01')
+from_serial
+# %%
+from_string = pd.to_datetime(kokyaku_data.loc[(~flg_is_digit), '登録日'])
+from_string
+# %%
+kokyaku_data['登録日'] = pd.concat([from_serial, from_string])
+kokyaku_data
+#%%
+kokyaku_data['登録年月'] = kokyaku_data['登録日'].dt.strftime('%y%m')
+result = kokyaku_data.groupby('登録日')
+print(result)
+# %%
